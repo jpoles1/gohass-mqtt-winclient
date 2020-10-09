7ed47e537227ffe9531b5c386d44586f02815782
Jordan Poles <jpdev.noreply@gmail.com>
2020-10-09 17:45:55 -0400

    Installer fix

--- install-in-startup.bat
+++ install-in-startup.bat
@@ -1,4 +1,4 @@
-copy "dist\gohass-mqtt-winclient.exe" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+copy /y "dist\gohass-mqtt-winclient.exe" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 copy "example.env" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\gohass-mqtt-winclient.env"
 explorer.exe "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 PAUSE
