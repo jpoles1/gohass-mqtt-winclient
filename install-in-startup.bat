a3003aabff391f695112d24f1576fb624f718387
Jordan Poles <jpdev.noreply@gmail.com>
2020-10-09 18:02:46 -0400

    Don't overwrite old config on re-install/update

--- install-in-startup.bat
+++ install-in-startup.bat
@@ -1,4 +1,4 @@
 copy /y "dist\gohass-mqtt-winclient.exe" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
-copy "example.env" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\gohass-mqtt-winclient.env"
+echo n | copy /-y "example.env" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\gohass-mqtt-winclient.env"
 explorer.exe "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 PAUSE
